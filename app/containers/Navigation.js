8a869d64ed75c9b27b062902e83c4c6484922f31
John Crygier <john.crygier@ventivtech.com>
2017-02-20 12:41:12 -0600

    Selectable List

--- app/containers/Navigation.js
+++ app/containers/Navigation.js
@@ -1,8 +1,8 @@
 // @flow
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import Drawer from 'material-ui/Drawer';
 import { connect } from 'react-redux';
-import { List, ListItem } from 'material-ui/List';
+import { List, ListItem, makeSelectable } from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import MdDashboard from 'react-icons/lib/md/dashboard';
 import MdDns from 'react-icons/lib/md/dns';
@@ -11,13 +11,49 @@ import MdCloudQueue from 'react-icons/lib/md/cloud-queue';
 import { browserHistory } from 'react-router';
 import icons from '../utils/icons';
 
+let SelectableList = makeSelectable(List);
+
+function wrapState(ComposedComponent) {
+  return class SelectableList extends Component {
+    static propTypes = {
+      children: PropTypes.node.isRequired,
+      defaultValue: PropTypes.number.isRequired,
+    };
+
+    componentWillMount() {
+      this.setState({
+        selectedIndex: this.props.defaultValue,
+      });
+    }
+
+    handleRequestChange = (event, index) => {
+      this.setState({
+        selectedIndex: index,
+      });
+    };
+
+    render() {
+      return (
+        <ComposedComponent
+          value={this.state.selectedIndex}
+          onChange={this.handleRequestChange}
+        >
+          {this.props.children}
+        </ComposedComponent>
+      );
+    }
+  };
+}
+
+SelectableList = wrapState(SelectableList);
+
 const Navigation = (props, context) => (
   <div>
     <Drawer open>
-      <List>
+      <SelectableList defaultValue="Dashboard">
         <Subheader>NAVIGATION</Subheader>
-        <ListItem primaryText="Dashboard" leftIcon={(<MdDashboard />)} />
-        <ListItem primaryText="Containers" leftIcon={(<MdDns />)} />
+        <ListItem value="Dashboard" primaryText="Dashboard" leftIcon={(<MdDashboard />)} />
+        <ListItem value="Containers" primaryText="Containers" leftIcon={(<MdDns />)} />
 
         <Subheader>CLUSTERS</Subheader>
         {props.servers.map(server => {
@@ -27,6 +63,7 @@ const Navigation = (props, context) => (
           return (
             <ListItem
               key={server.id}
+              value={server.id}
               primaryText={server.name}
               leftIcon={(<Icon style={{ color: server.connected ? 'green' : 'red' }} />)}
               nestedItems={renderTierList(server, cluster, props)}
@@ -34,7 +71,7 @@ const Navigation = (props, context) => (
             />
           );
         })}
-      </List>
+      </SelectableList>
     </Drawer>
   </div>
 );
@@ -46,6 +83,7 @@ const renderTierList = (server, cluster, props) => {
       return (
         <ListItem
           key={tierName}
+          value={tierName}
           primaryText={tierName}
           leftIcon={(<MdCloudQueue />)}
           nestedItems={renderEnvironmentList(server, tierName, cluster, tier, props)}
@@ -64,6 +102,7 @@ const renderEnvironmentList = (server, tierName, cluster, tier, props) => (
   tier.map(environment => (
     <ListItem
       key={environment.id}
+      value={environment.id}
       primaryText={environment.description}
       leftIcon={(<MdDevicesOther />)}
       onTouchTap={() => props.router.push(`/clusters/${server.id}/${tierName}/${environment.id}`)}
